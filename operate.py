5ce48f67ee9d1f5a02f0b7b97fa25b99c54f1a91
yijiangw <yijiang.wang@hotmail.com>
2020-12-14 21:51:11 -0500

    s

--- operate.py
+++ operate.py
@@ -49,6 +49,9 @@ if __name__=='__main__':
     elif request=='key':
         for device_name in name_list:
             processes.add(subprocess.Popen(['python', 'func.key.py', device_name, sys.argv[3]]))
+    elif request=='carmera':
+        for device_name in name_list:
+            processes.add(subprocess.Popen(['python', 'func.carmera.py', device_name]))
     elif request=='exception':
         for device_name in name_list:
             processes.add(subprocess.Popen(['python', 'func.exception.py', device_name]))
